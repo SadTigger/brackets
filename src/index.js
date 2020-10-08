912b98874eb920ad535c7677ba72fbe97a917912
Pavel Zhachko <rammiel@yandex.ru>
2020-10-09 02:28:27 +0300

    feat: add check() functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketsPatterns = bracketsConfig.map(el => el.join(''));
+  let position = 0;
+  while (position < bracketsConfig.length ) {
+      let currentBracket = bracketsPatterns[position];
+      if (str.includes(currentBracket)) {
+          str = str.replace(currentBracket,'');
+          position = 0;
+      } else {
+          position++;
+      }
+  }
+  return str.length === 0;
+};
